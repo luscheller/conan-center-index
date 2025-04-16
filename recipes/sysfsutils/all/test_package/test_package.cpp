e2fdb6934ea33f5132a79c251757a4a5b4bd742d
lukas <lukas.scheller@kit.edu>
2025-04-16 14:27:27 +0200

    Add sysfsutils recipe

---
+++ recipes/sysfsutils/all/test_package/test_package.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "sysfs/libsysfs.h"
+
+int main(void) {
+    char sysfs_mntpath[SYSFS_PATH_MAX];
+    int rc = sysfs_get_mnt_path(sysfs_mntpath, SYSFS_PATH_MAX);
+    if (rc < 0) {
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
+}
